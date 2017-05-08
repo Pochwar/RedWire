f90e886c84d1e523500a3006427c427d3b32c022
Benoît Ripoche <ripoche.b@gmail.com>
2017-05-08 20:04:51 +0200

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import TVDB from 'node-tvdb';
 const tvdb = new TVDB('207403D377FCA987');
 
-tvdb.getSeriesByName('The Simpsons')
+
+tvdb.getSeriesByName('Breaking Bad')
     .then(response => {
+        console.log("### Search ###")
         console.dir(response);
+    })
+    .catch(error => { /* handle error */ });
+
+//get a serie's info + episodes
+tvdb.getSeriesAllById(71663)
+    .then(response => {
+        console.log("### Serie's info & episodes ###")
+        //console.dir(response);
     })
     .catch(error => { /* handle error */ });
