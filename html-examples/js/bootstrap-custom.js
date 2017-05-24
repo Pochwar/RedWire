d8ad2fa4c82ed2421eae7b8807d8a6f4cbfcfdc2
gabriel <pour.blaireaux@yahoo.fr>
2017-05-24 10:07:33 +0200

    started html-examples

---
+++ html-examples/js/bootstrap-custom.js
@@ -0,0 +1,30 @@
+// Plugin for bootstrap library
+(function(){
+
+    function BootstrapPlugin(){}
+
+    BootstrapPlugin.prototype.initNavigation = function(){
+    
+        // les liens page-scroll déclenchent un scroll animé
+        $('.page-scroll a').bind('click', function(event) {
+
+            // cible & vitesse
+            var target = $(this).attr('href');
+            var speed = 750;
+
+            // Pour éviter le clignotement dans IE / Firefox, ...
+            event.preventDefault();
+
+            // fermeture du menu déroulant
+            $('.navbar-toggle:visible').click();
+
+            // animation
+            $('html, body').animate( { scrollTop: $(target).offset().top }, speed ); 
+        });
+    }
+
+    // affect plugin to window
+    this.BootstrapPlugin = new BootstrapPlugin();
+})();
+
+
