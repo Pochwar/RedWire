c6b2ad15d65b71ec1f16ddf0bc79761b73a251ff
Pochwar <ripoche.b@gmail.com>
2017-06-09 09:46:09 +0200

    Create IndexCtrl.js

--- src/controllers/IndexCtrl.js
+++ src/controllers/IndexCtrl.js
@@ -1,18 +1,6 @@
 class IndexCtrl {
     static indexLoggedAction(req, res) {
-        res.render('indexAuthenticated.twig', {
-            pseudo: req.session.user.pseudo,
-        });
-    }
-
-    get(req, res) {
-        let lang = "fr";
-        
-        if(req.cookies.i18n){lang = req.cookies.i18n}
-
-        res.render('indexUnauthenticated.twig', {
-            lang: lang,
-        })
+        res.render('indexAuthenticated.twig');
     }
 }
 
