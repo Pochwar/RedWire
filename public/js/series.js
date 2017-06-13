ee0dd79ac21b64afdf546fdc65f32ccd40a8b1a6
gabriel <pour.blaireaux@yahoo.fr>
2017-06-13 09:16:24 +0200

    working on search

--- public/js/series.js
+++ public/js/series.js
@@ -3,12 +3,38 @@ var expandPanel = function (index) {
     // get data
     var title = $('#data-'+index+' .data-title').text();
     var overview = $('#data-'+index+' .data-overview').text();
-    var img = $('#poster-'+index).src;
+    var img = $('#poster-'+index).attr('src');
+
+    $(".poster-list").removeClass("ct-border-red").each(function () {
+        console.log(this);
+        if ($(this).next().length && $(this).offset().top < $(this).next().offset().top) {
+            $(this).addClass("ct-border-red");
+        }
+    });
 
     // update panel
     $('#panel-title').text(title);
     $('#panel-overview').text(overview);
     $('#panel-poster').attr("src", img);
     $('#panel-poster').attr("alt", title);
-     $('#panel-poster').attr("title", title);
+    $('#panel-poster').attr("title", title);
 }
+
+var timeout;
+$(window).on("load resize", function () {
+    console.log("event debounced");
+    if (timeout) {
+        window.clearTimeout(timeout);
+    }
+    timeout = setTimeout(function () {
+        console.log("resizing");
+        $(".poster-list").removeClass("ct-border-red").each(function () {
+            if ($(this).next().length && $(this).offset().top < $(this).next().offset().top) {
+                $(this).addClass("ct-border-red");
+            }
+            if ($(this).prev().length && $(this).offset().top > $(this).prev().offset().top) {
+                //$(this).addClass("first");
+            }
+        });
+    }, 100);
+});
