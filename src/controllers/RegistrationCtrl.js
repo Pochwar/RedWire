98579d5ab012142a824ab2fc3984abca9e6ae194
Benoît Ripoche <ripoche.b@gmail.com>
2017-06-13 15:09:01 +0200

    Fix date bug - in registration - in user modification

--- src/controllers/RegistrationCtrl.js
+++ src/controllers/RegistrationCtrl.js
@@ -35,6 +35,7 @@ class RegistrationCtrl {
                 status: 500,
                 error: res.__('ERROR_EMPTY'),
             });
+            return;
         }
 
         //check passwords
@@ -43,6 +44,7 @@ class RegistrationCtrl {
                 status: 500,
                 error: res.__('ERROR_PASS'),
             });
+            return;
         }
 
         //check db connection
@@ -51,20 +53,35 @@ class RegistrationCtrl {
                 status: 500,
                 error: res.__('ERROR_SERVER'),
             });
+            return;
         }
 
         //CREATE NEW USER
         const user = new UserModel();
+
         //get date of the day
         // const now = new Date();
         // const day = now.getDate();
         // const month = now.getMonth() + 1;
         // const year = now.getFullYear();
         // const createdAt = `${day}/${month}/${year}`;
-        const createdAt = Date.now();
+        const createdAt = new Date();
+
         //set default ban status and role id
         const ban = this._conf.site.default.ban;
         const roleId = this._conf.site.default.role;
+
+        //check and format birthday
+        var dateRegex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+        if(!req.body.birthday.match(dateRegex)){
+            res.render('registration.twig', {
+                msg: res.__('BIRTHDAY_INVALID'),
+            });
+            return;
+        }
+        const birthdayArray = req.body.birthday.split("/");
+        const birthday = new Date(birthdayArray[2], (birthdayArray[1]-1), birthdayArray[0]);
+
         //hash password
         const saltRounds = 10;
         encrypt.hash(req.body.password, saltRounds, (err, hash) => {
@@ -72,7 +89,7 @@ class RegistrationCtrl {
                     req.body.firstname,
                     req.body.lastname,
                     req.body.pseudo,
-                    req.body.birthday,
+                    birthday,
                     req.body.mail,
                     createdAt,
                     hash,
@@ -94,16 +111,19 @@ class RegistrationCtrl {
                                 status: 500,
                                 error: res.__('ERROR_PSEUDO'),
                             });
+                            return;
                         } else if (err.message.match(/mail/i)) {
                             res.render('registration.twig', {
                                 status: 500,
                                 error: res.__('ERROR_MAIL'),
                             });
+                            return;
                         } else {
                             res.render('registration.twig', {
                                 status: 500,
                                 error: res.__('ERROR_SERVER'),
                             });
+                            return;
                         }
                     }
                     res.render('registration.twig', {
@@ -112,7 +132,6 @@ class RegistrationCtrl {
                     });
                 });
         })
-
     }
 
 }
--- src/controllers/UserCtrl.js
+++ src/controllers/UserCtrl.js
@@ -89,6 +89,30 @@ class UserCtrl {
                 });
         }
 
+        //change date
+        else if(dataToChange === "birthday"){
+            var dateRegex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+            if(!req.body.birthday.match(dateRegex)){
+                res.redirect('/user?msg=birthdayError');
+            }
+
+            winston.info(req.body.birthday);
+            const birthdayArray = req.body.birthday.split("/");
+            const birthday = new Date(birthdayArray[2],(birthdayArray[1]-1), birthdayArray[0]);
+            winston.info(birthday);
+
+            user.updateData(userId, dataToChange, birthday)
+                .then(document => {
+                    winston.info("ok")
+                    res.redirect('/user?msg='+dataToChange);
+                })
+                .catch(err => {
+                    winston.info(`error : ${err}`)
+                    res.redirect('/user?msg='+dataToChange+'Error');
+                })
+            ;
+        }
+
         //change anything else
         else {
             winston.info(`${dataToChange} : ${req.body.value}`)
