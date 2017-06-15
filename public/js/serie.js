b1c11b46421240609df3a6c32dc482d675787cfb
EveX1 <evekill@free.fr>
2017-06-15 15:35:54 +0200

    finished episodes viewed !!!!!!!

--- public/js/serie.js
+++ public/js/serie.js
@@ -1,25 +1,51 @@
 $(document).ready(function() {
-    console.log('serie.js loaded');
 
     $(".viewed").click(function(e) {
         e.preventDefault();
-        console.log(e.target.id);
 
-        var data = {
-            id: e.target.id,
-        }
+		var button = $("#" + e.target.id);
 
-	    $.ajax({
-	       url : '/login', 
-	       type : 'PUT', 
-	       data : data,
-	       success: response => {
-	       	console.log(response);
-	       },
-	       error: error => {
-	       	 console.log(error);
-	       }
-	    });
+		if (button.hasClass("btn-warning")) {
+			button.removeClass("btn-warning");
+			button.addClass("btn-success");
+			button.html("&#10003; Vu");
+
+			$.ajax({
+				url : '/user', 
+				type : 'PUT', 
+				data : {
+					episodeId: e.target.id,
+					remove: false,
+				},
+			success: response => {
+				// console.log(response);
+				},
+			error: error => {
+				console.log(error);
+				}
+			});
+
+		} else {
+			button.removeClass("btn-success");
+			button.addClass("btn-warning");
+			button.html("Pas vu");
+
+			$.ajax({
+				url : '/user', 
+				type : 'PUT', 
+				data : {
+					episodeId: button.id,
+					remove: true,
+				},
+			success: response => {
+				// console.log(response);
+				},
+			error: error => {
+				console.log(error);
+				}
+			});
+		}
+		
 
     });
 });
