a58159da7f9e35a6f0348cd59d509518190da9be
gabriel <pour.blaireaux@yahoo.fr>
2017-06-16 17:04:16 +0200

    fixed avatar bugs

--- config/config.js
+++ config/config.js
@@ -19,8 +19,8 @@ const CONF = {
             avatarPath: process.env.CONF_SITE_AVATARPATH,
             resultPerPage: 10,
             poster: '/img/poster-default.png',
+            avatar: '/img/user.png',
             ageMax : 31556952000 * 13, //13 years,
-            contact: process.env.CONF_MAIL_CONTACT,
         },
         roles: {
             user: 1,
